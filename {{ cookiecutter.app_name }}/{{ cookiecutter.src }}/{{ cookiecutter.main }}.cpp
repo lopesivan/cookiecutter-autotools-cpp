b1ce5fc8a9dada055eed6eba0e080e1e28aebbb8
Ivan Lopes <lopesivan.ufrj@gmail.com>
2023-10-23 18:08:52 -0300

    Atualiza o arquivo "{{@cookiecutter.app_name@}}/{{@cookiecutter.src@}}/{{@cookiecutter.main@}}.cpp"

--- {{ cookiecutter.app_name }}/{{ cookiecutter.src }}/{{ cookiecutter.main }}.cpp
+++ {{ cookiecutter.app_name }}/{{ cookiecutter.src }}/{{ cookiecutter.main }}.cpp
@@ -2,7 +2,7 @@
 #include "{{ cookiecutter.help_file_name }}.h"
 #include "../{{ cookiecutter.feature }}/{{ cookiecutter.feature_file_name }}.h"
 
-int {{ cookiecutter.main }}() {
+int main() {
     {{ cookiecutter.feature_class_name }} {{ cookiecutter.feature_method }}_obj;
     std::cout << "Addition Result:"
               << {{ cookiecutter.feature_method }}_obj.{{ cookiecutter.feature_method }}(1,2)
